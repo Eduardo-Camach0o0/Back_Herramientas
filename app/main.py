24bcdb732a98372cbbdcf42a3a474a24e6cf0a42
Eduardo Camacho <eduardochavez010604@hotmail.com>
2024-11-21 12:29:28 -0600

    llama y servidor

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-print("Hola")
+import ollama
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+modelo = "llama3.2"
+
+historia = [{'role':"system","content":"Eres una IA, para responder sola y unicamente preguntas de la clase de herramientas para el procesamiento de IA"}]
+
+
+
+@app.post("/mensaje")
+def init():
+    data = request.get_json()
+    mensaje = data["mensaje"]
+
+    historia.append({'role':"user","content":mensaje})
+    respuestas = ollama.chat(model = modelo, messages=historia)
+    historia.append({'role':"system","content":respuestas["message"]["content"]})
+
+   
+
+    return jsonify({"respuesta": respuestas["message"]["content"]}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
