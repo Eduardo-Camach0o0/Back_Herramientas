4ad26c7d663b4e7843abab2cb48b76d0e048df2a
Eduardo Camacho <eduardochavez010604@hotmail.com>
2024-12-01 16:58:55 -0600

    Back terminado
    Se actualizo los requerimientos. Se implemento el pdf y la busqueda por caracteres, por lo que tarda un poco más en responder

--- app/main.py
+++ app/main.py
@@ -1,12 +1,43 @@
 import ollama
 from flask import Flask, jsonify, request
+import pdfplumber
+
 
 app = Flask(__name__)
 
 
+
+
+
+
+
+# def extraer_texto_pdf(ruta_pdf):
+#     texto = ""
+#     with pdfplumber.open(ruta_pdf) as pdf:
+#         for pagina in pdf.pages:
+#             texto += pagina.extract_text()
+
+#     print("Texto extraído del PDF:")
+#     print(texto)
+#     return texto
+
+
+
+#texto_pdf = extraer_texto_pdf("src/recursos.pdf")
+
+
+
+
 modelo = "llama3.2"
 
-historia = [{'role':"system","content":"Eres una IA, para responder sola y unicamente preguntas de la clase de herramientas para el procesamiento de IA"}]
+# historia = [
+#     {'role': 'system', 'content': f"Eres una IA, responde solo preguntas basándote en el siguiente texto: {texto_pdf}, sé breve y solo responde, segun el texto"}
+# ]
+
+historia = [
+    {'role': 'system', 'content': f"Eres una IA, responde que eres experto en la Materia de Herramientas Para Ingenieria en I.A, sé breve y solo responde preguntas de esa indole, no más"}
+]
+
 
 
 
@@ -14,6 +45,8 @@ historia = [{'role':"system","content":"Eres una IA, para responder sola y unica
 def init():
     data = request.get_json()
     mensaje = data["mensaje"]
+
+    print(mensaje)
 
     historia.append({'role':"user","content":mensaje})
     respuestas = ollama.chat(model = modelo, messages=historia)
